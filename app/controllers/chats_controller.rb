a833b03ac28185c5fc98c932ae449b669dccc1b5
EC2 Default User <ec2-user@ip-172-31-44-103.ap-northeast-1.compute.internal>
2020-04-22 14:51:19 +0000

    has_manyとbelongs_to　追加

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,6 +1,9 @@
 class ChatsController < ApplicationController
-   def show
-    　@channels = Channel.find_by(id:params[:id])
-    　@chats = Chat.where(channel_id: @channel.id)
-   end
+    def show
+      @channel = Channel.find_by(id:params[:id])
+      @chats.channel = @channel
+        #   @user = User.where(user_id: @chats.user_id)
+        # @channel = Channel.find_by(id:params[:id])
+        #   @chats = Chat.where(channel_id: @channel.id)
+    end
 end
