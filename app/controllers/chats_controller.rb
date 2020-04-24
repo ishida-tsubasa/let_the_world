6c065f3a8a545736419ddb05fe7c85b7642c974d
EC2 Default User <ec2-user@ip-172-31-44-103.ap-northeast-1.compute.internal>
2020-04-24 13:30:30 +0000

    chat画面　修正

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,6 +2,6 @@ class ChatsController < ApplicationController
     def show
       @channel = Channel.find_by(id:params[:id])
     #   @chats = Chat.where(channel_id: @channel.id) 左記のコードを下記に変更
-      @chats = @channel.chats
+      @chats = @channel.chats.includes(:user)
     end
 end
